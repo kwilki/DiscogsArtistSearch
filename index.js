f2a1ca9ca08ec8be8ede5a74aa2060c0a420213d
Kyle <kylewilkinson14@gmail.com>
2021-11-25 17:31:31 +1100

    releases working but not rendering

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+// API Requirements
 const apiKey = "key=BvZVUiBAAUkaPKFlsowt"
 const secret = "secret=FGcSHfPOplxpojCdQmjjZOEgMUDEVRRG"
 
 const form = document.querySelector("#search-form")
 const input = document.querySelector("#artist-input")
-
 
 form.addEventListener("submit", function(event) {
     event.preventDefault()
@@ -12,6 +12,7 @@ form.addEventListener("submit", function(event) {
     input.value = "";
 })
 
+// initial Discogs Fetch
 function searchArtist(artist) {
     const url = `https://api.discogs.com/database/search?q=${artist}&${apiKey}&${secret}&per_page=200`
     const information = document.querySelector("#information")
@@ -19,29 +20,14 @@ function searchArtist(artist) {
     fetch(url)
     .then(response => response.json())
     .then(function(json) {
-        console.log(json)
         const search = json
         console.log(search.results)
         console.log(search.results.find(x => x.type === "artist"))
         artist = search.results.find(x => x.type === "artist")
         const newUrl = artist.resource_url
-        console.log(newUrl)
+        const coverImg = artist.cover_image // This is where the cover image comes from
         fetchArtistInfo(newUrl)
     })
-}
-
-function renderArtist(data) {
-    console.log(data)
-    const toRender = document.querySelector("#information")
-    const dataDisplay = document.createElement("div")
-    dataDisplay.innerHTML = 
-    `<h2>${data.name}</h2>
-    <p>Real Name: <em>${data.realname}</em></p>
-    <h4>About</h4>
-    <p>${data.profile}</p>
-    <button onclick="goToReleases(${data.releases_url})">Releases</button>`
-    toRender.append(dataDisplay)
-    console.log(data.releases_url)
 }
 
 function fetchArtistInfo(newUrl) {
@@ -50,18 +36,47 @@ function fetchArtistInfo(newUrl) {
     .then(renderArtist)
 }
 
-function goToReleases(releases) {
-    alert("Clicked!")
-    fetch(releases)
+function renderArtist(data) {
+    console.log(data)
+
+    const toRender = document.querySelector("#information")
+    const dataDisplay = document.createElement("div")
+    const associatedUrls = data.urls
+
+    dataDisplay.innerHTML = // this is where cover img needs to go
+    `<h2>${data.name}</h2>
+    <img src="">
+    <p>Real Name: <em>${data.realname}</em></p>
+    <h4>About</h4>
+    <p>${data.profile}</p>
+    <h4>Links</h4>
+    <ul>${renderAssociatedUrls(associatedUrls).join("")}</ul>
+    <button data-url=${data.releases_url} onclick="goToReleases(event)">Releases</button>`
+
+    toRender.append(dataDisplay)
+}
+
+function renderAssociatedUrls(associatedUrls) {
+    return associatedUrls.map(url => `<li><a href="${url}">${url}</a></li>`)
+}
+
+function goToReleases(event) {
+    const button = event.target
+    const url = button.dataset["url"]
+    fetch(url)
     .then(releases => releases.json())
-    .then(function(json){
-        console.log(json)
+    .then(function(releasesJson) {
+        console.log(releasesJson)
+        renderReleases(releasesJson) // this is where the artist releases come from
     })
 }
 
-function renderReleases() {
+function renderReleases(releasesJson) {
     const toRender = document.querySelector("#information")
     const dataDisplay = document.createElement("div")
+    const releases = releasesJson.releases.filter(release => (release.type === "master"))
+    
+    console.log(releases)
 }
 
 
