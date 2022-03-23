d9a946e97ef6607572da0bb69c16b3e1a22ff5db
kwilki <kylewilkinson14@gmail.com>
2022-03-23 20:32:37 +1100

    initial search working and rendering

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const formBody = document.querySelector("#search-body")
 const input = document.querySelector("#artist-input")
 
 const pageHeading = document.querySelector("#page-heading")
-const main = document.querySelector("#information")
+const information = document.querySelector("#information")
 const dataDisplay = document.createElement("div")
+const artistPicture = document.querySelector("#cover-img")
 
 form.addEventListener("submit", function(event) {
     event.preventDefault()
@@ -36,4 +37,64 @@ function searchArtist(artist) {
         fetchArtistInfo(newUrl)
         renderCoverImg(coverImg)
     })
+}
+
+function fetchArtistInfo(newUrl) {
+    fetch(newUrl)
+    .then(response => response.json())
+    .then(renderArtist)
+}
+
+function renderCoverImg(coverImg) {
+    const imgToRender = `<img src="${coverImg}">`
+    artistPicture.innerHTML = imgToRender
+}
+
+function renderArtist(data) {
+    console.log(data)
+
+    const associatedUrls = data.urls
+    const members = data.members
+    const artistName = data.name
+    // const url = data.members.resource_url // this is where the members apis are
+    // console.log(url) 
+    artistReleases = data.releases_url
+
+    pageHeading.innerHTML =  `<h2 class="artist-name">${data.name}</h2>`
+
+    dataDisplay.innerHTML =
+    `<button data-url=${artistReleases} onclick="goToReleases(event)">Artist Releases</button>
+    <button id="favourite-artist" data-name=${artistName}>Favourite</button>
+    <p>Real Name: <em>${data.realname}</em></p>
+    <h4>Members:</h4>
+    <ul>${renderMembers(members, artistName)}</ul>
+    <h4>About</h4>
+    <p class="about">${data.profile}</p>
+    <div class="links">
+        <h4>Links</h4>
+        
+        <ul>
+            <li><a href="${data.uri}" target="_blank">Dicogs Page</a></li>
+            ${renderAssociatedUrls(associatedUrls)}
+        </ul>
+    </div>
+    `
+    console.log(artistName)
+    information.append(dataDisplay)
+    prevPage = information.innerHTML
+}
+
+function renderAssociatedUrls(associatedUrls) {
+    if (associatedUrls) {
+        return associatedUrls.map(url => `<li><a href="${url}" target="_blank">${url}</a></li>`).join("")
+    } else return 'None to display'
+    
+}
+
+function renderMembers(members, artistName) {
+    if (members){
+        return members.map(current => `<li><strong>${current.name}</strong></li>
+        Active: ${current.active}`).join("")
+    } else return `${artistName}`
+    
 }
